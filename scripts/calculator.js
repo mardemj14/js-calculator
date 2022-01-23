8985176fbbc27a783ddbab859b58c2fc39c3f84b
Demar Jones <demarjones14@outlook.com>
2022-01-23 09:56:11 -0500

    add basic math operation function

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,1 +1,7 @@
 "use strict";
+
+const add = (n1, n2) => n1 + n2;
+const subtract = (n1, n2) => n1 - n2;
+const divide = (n1, n2) => n1 / n2;
+const multiply = (n1, n2) => n1 * n2;
+const operate = (operator, n1, n2) => operator(n1, n2);
