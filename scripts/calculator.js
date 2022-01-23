8156956b9fcb7a6e67120458f83016ee44380e07
Demar Jones <demarjones14@outlook.com>
2022-01-23 15:06:21 -0500

    Calculator is now functional

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,17 +1,33 @@
 'use strict';
 
 const displayTextEl = document.querySelector('.display-text');
+
 let displayValue;
+let firstNum;
+let currentOperator;
+let isRunningTotal = false;
+
+console.log(firstNum);
 
 const add = (n1, n2) => n1 + n2;
 const subtract = (n1, n2) => n1 - n2;
 const divide = (n1, n2) => n1 / n2;
 const multiply = (n1, n2) => n1 * n2;
-const operate = (operator, n1, n2) => operator(n1, n2);
+
+// rounds to 3 decimal places
+const operate = (operator, n1, n2) => Math.round(operator(n1, n2) * 1000) / 1000;
+
 const buttons = document.querySelectorAll('.btn');
 
+const operator = {
+  '+': add,
+  x: multiply,
+  '-': subtract,
+  '/': divide,
+};
+
 function updateDisplay(value) {
-  displayTextEl.textContent += value.innerHTML;
+  displayTextEl.textContent += value;
   displayValue = Number(displayTextEl.textContent);
 }
 
@@ -25,8 +41,36 @@ for (let i = 0; i < buttons.length; i++) {
     const isClear = this.classList.contains('clr');
     const isDelete = this.classList.contains('del');
 
+    // if a number button is pressed
     if (isNumber) {
-      updateDisplay(this);
+      if (isRunningTotal) {
+        displayTextEl.textContent = '';
+        updateDisplay(this.innerHTML);
+      } else {
+        updateDisplay(this.innerHTML);
+      }
+    }
+    // if an operator button is pressed
+    else if (isOperator) {
+      if (firstNum !== undefined) {
+        displayTextEl.textContent = '';
+        updateDisplay(operate(operator[currentOperator], firstNum, displayValue));
+
+        firstNum = displayValue;
+        currentOperator = this.innerHTML;
+        isRunningTotal = true;
+      } else {
+        firstNum = displayValue;
+        currentOperator = this.innerHTML;
+        displayTextEl.textContent = '';
+      }
+    }
+    // if the enter button is pressed
+    else if (isEnter) {
+      displayTextEl.textContent = '';
+      updateDisplay(operate(operator[currentOperator], firstNum, displayValue));
+      firstNum = undefined;
+      isRunningTotal = true;
     }
   });
 }
